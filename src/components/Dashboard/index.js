1af8575fd90d916097204f1da2d943d12eed2fbd
Smarta <sm3rta@yahoo.com>
2019-03-09 18:53:34 +0200

    fix

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
-import { Bar } from "react-chartjs-2";
+import Bar from "./Bar";
 
 export default class Dashboard extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-6">
-                        <Bar></Bar>
-                    </div>
-                    <div className="col-6">
-                        <Bar></Bar>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <Bar />
+          </div>
+          <div className="col-6">
+            <Bar />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
